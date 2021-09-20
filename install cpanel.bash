38e99d199f75f8f6605a3add86616bda7151139d
rahul amin roktim <rahulaminroktim@gmail.com>
2021-09-20 14:43:12 +0600

    Update install cpanel.bash

--- install cpanel.bash
+++ install cpanel.bash
@@ -5,5 +5,5 @@ sudo yum install perl
 sudo yum install curl
 sudo curl -o latest -L https://securedownloads.cpanel.net/latest
 sudo sh latest
-sudo chkconfig network on
-sudo service network start
+sudo systemctl enable network.service
+sudo systemctl start network.service

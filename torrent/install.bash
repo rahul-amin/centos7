e51e2f5b13af64d8f1529efac2ab753b1c56f94b
rahul amin roktim <rahulaminroktim@gmail.com>
2021-09-20 19:04:53 +0600

    Create install.bash

---
+++ torrent/install.bash
@@ -0,0 +1,8 @@
+sudo yum install transmission-cli
+
+sudo yum install -y aria2
+# aria2c http://gtf.org/garzik/bitcoin/bootstrap.dat.torrent
+# aria2c 'magnet:?xt=urn:btih:248D0A1CD08284299DE78D5C1ED359BB46717D8C'
+
+sudo yum install rtorrent
+#

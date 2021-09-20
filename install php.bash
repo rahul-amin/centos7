aeaac1af162e33fdd05b4c316e05b8ff5b5bdc2e
rahul amin roktim <rahulaminroktim@gmail.com>
2021-09-20 11:22:19 +0600

    Update install php.bash

--- install php.bash
+++ install php.bash
@@ -4,3 +4,14 @@ sudo yum update
 sudo yum install php
 sudo yum install php php-common php-opcache php-mcrypt php-cli php-gd php-curl php-mysql -y
 sudo systemctl start httpd
+
+
+sudo systemctl enable httpd.service
+sudo yum install mariadb-server mariadb
+sudo systemctl start mariadb
+sudo mysql_secure_installation
+sudo systemctl enable mariadb.service
+
+sudo yum install php php-mysql
+sudo systemctl restart httpd.service
+sudo yum install php-fpm

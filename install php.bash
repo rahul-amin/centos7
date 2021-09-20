ce304b6b989a02d11912a778920d7e503daed7d0
rahul amin roktim <rahulaminroktim@gmail.com>
2021-09-20 11:12:32 +0600

    Update install php.bash

--- install php.bash
+++ install php.bash
@@ -1,4 +1,6 @@
 sudo yum -y install epel-release
-sudo yum-config-manager --enable remi-php74
+sudo yum -config-manager --enable remi-php74
 sudo yum update
 sudo yum install php
+sudo yum install php php-common php-opcache php-mcrypt php-cli php-gd php-curl php-mysql -y
+sudo systemctl start httpd

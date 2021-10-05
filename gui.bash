532bb49792e2947a24344402ab6359deeca87085
rahul amin roktim <rahulaminroktim@gmail.com>
2021-10-05 14:09:05 +0600

    Update gui.bash

--- gui.bash
+++ gui.bash
@@ -10,6 +10,6 @@ sudo yum -y groupinstall X11
 sudo yum --enablerepo=epel -y groups install "Xfce"
 echo "exec /usr/bin/xfce4-session" >> ~/.xinitrc
 
-#sudo yum --enablerepo=epel -y groups install "MATE Desktop"
-#echo "exec /usr/bin/mate-session" >> ~/.xinitrc
+sudo yum --enablerepo=epel -y groups install "MATE Desktop"
+echo "exec /usr/bin/mate-session" >> ~/.xinitrc
 

e2767927cd4a8d5acbe878f4e15e71a1e14b7a75
rahul amin roktim <rahulaminroktim@gmail.com>
2021-10-05 14:05:58 +0600

    Create gui.bash
    adding GUI to centos 7 commands

---
+++ gui.bash
@@ -0,0 +1,15 @@
+sudo yum -y groups install "GNOME Desktop"
+echo "exec gnome-session" >> ~/.xinitrc
+
+systemctl set-default graphical.target
+sudo yum -y groups install "KDE Plasma Workspaces"
+echo "exec startkde" >> ~/.xinitrc
+
+
+sudo yum -y groupinstall X11
+sudo yum --enablerepo=epel -y groups install "Xfce"
+echo "exec /usr/bin/xfce4-session" >> ~/.xinitrc
+
+#sudo yum --enablerepo=epel -y groups install "MATE Desktop"
+#echo "exec /usr/bin/mate-session" >> ~/.xinitrc
+
